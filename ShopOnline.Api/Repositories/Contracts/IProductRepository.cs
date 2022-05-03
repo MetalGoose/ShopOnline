0419cde3874933c6bf88e91d7db2e8c3c7e66607
Евгений Беляков <freelans73@gmail.com>
2022-05-03 23:47:41 +0300

    Added product repository and controller

---
+++ ShopOnline.Api/Repositories/Contracts/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShopOnline.Api.Entities;
+
+namespace ShopOnline.Api.Repositories.Contracts
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetItemAsync(int id);
+        Task<ProductCategory> GetCategoryAsync(int id);
+        Task<IEnumerable<Product>> GetItemsAsync();
+        Task<IEnumerable<ProductCategory>> GetCategoriesAsync();
+    }
+}
---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext _shopOnlineDbContext;
+
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            _shopOnlineDbContext = shopOnlineDbContext;
+        }
+
+        public async Task<IEnumerable<ProductCategory>> GetCategoriesAsync()
+        {
+            var categories = await _shopOnlineDbContext.ProductCategories.ToListAsync();
+            return categories;
+        }
+
+        public Task<ProductCategory> GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetItemAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItemsAsync()
+        {
+            var products = await _shopOnlineDbContext.Products.ToListAsync();
+            return products;
+        }
+    }
+}
